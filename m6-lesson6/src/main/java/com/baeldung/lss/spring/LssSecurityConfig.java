1010028439a5e3d23313b26585ece3d2d3360336
Ger Roza <rozagerardo@gmail.com>
2024-04-12 21:34:43 -0300

    Upgraded to Boot 3.2

--- m6-lesson6/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
+++ m6-lesson6/src/main/java/com/baeldung/lss/spring/LssSecurityConfig.java
@@ -50,24 +50,22 @@ public class LssSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {// @formatter:off
         http
-        .authorizeHttpRequests()
+        .authorizeHttpRequests((authorize) -> authorize
                 .requestMatchers("/signup",
                         "/user/register",
                         "/registrationConfirm*",
                         "/badUser*",
                         "/js/**").permitAll()
-                .anyRequest().authenticated()
+                .anyRequest().authenticated())
 
-        .and()
-        .formLogin().
-            loginPage("/login").permitAll().
-            loginProcessingUrl("/doLogin")
+        .formLogin((form) -> form
+                .loginPage("/login").permitAll()
+                .loginProcessingUrl("/doLogin"))
 
-        .and()
-        .logout().permitAll().logoutUrl("/logout")
+        .logout((logout) -> logout
+                .permitAll().logoutUrl("/logout"))
 
-        .and()
-        .csrf().disable();
+        .csrf((csrf) -> csrf.disable());
         return http.build();
     } // @formatter:on
 
